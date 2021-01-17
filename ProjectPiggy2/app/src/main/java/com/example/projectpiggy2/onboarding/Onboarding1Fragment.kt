698fbe660ca7234740f5285e3df91d16f0308f35
Flora Guo <floraguolr@gmail.com>
2021-01-16 18:11:11 -0800

    Revert "Revert "Merge branch 'main' into allowance""
    This reverts commit d5654818910669bc85cc334ba8e0e0ff5f6abc47.

---
+++ ProjectPiggy2/app/src/main/java/com/example/projectpiggy2/onboarding/Onboarding1Fragment.kt
@@ -0,0 +1,22 @@
+package com.example.projectpiggy2.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.example.projectpiggy2.R
+
+class Onboarding1Fragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_onboarding1, container, false)
+    }
+}
